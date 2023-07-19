4a21b6bbabe5565f6567ffd782adec6c4a492d70
andres-condezo <ra.condezomonge@gmail.com>
2023-07-19 00:58:28 -0500

    [Feat][Add icons and functionality to the slider]

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,41 @@
-import Head from 'next/head'
+'use client'
+
+import Head from 'next/head';
+import { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
 const Home = () => {
   const slides = [
     {
-      url: 'http://placehold.it/900x350',
+      url: 'https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80',
     },
     {
-      url: 'http://placehold.it/900x350',
+      url: 'https://images.unsplash.com/photo-1594909122845-11baa439b7bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1615986201152-7686a4867f30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=725&q=80',
     },
     {
       url: 'http://placehold.it/900x350',
     },
   ];
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length -1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  }
 
   return (
   <div>
@@ -70,25 +94,21 @@ const Home = () => {
       </div>
 
       <div className="w-full">
-        <div id="default-carousel" className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative" >
-          <div style={{backgroundImage: `url(${slides[0].url})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-5--">
-        </div>
-        <button type="button" className="absolute top-[50%] -translate-x-0 translate-y[-50%] left-5">
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg size={30} className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 1 1 5l4 4"/>
-                </svg>
-                <span className="sr-only">previous</span>
-            </span>
-        </button>
-        <button type="button" className="absolute top-[50%] -translate-x-0 translate-y[-50%] left-5">
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg size={30} className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                </svg>
-                <span className="sr-only">next</span>
-            </span>
-        </button>
+        <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group" >
+          <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-300"></div>
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            <BsChevronCompactLeft onClick={prevSlide} size={30}/>
+          </div>
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            <BsChevronCompactRight onClick={nextSlide} size={30}/>
+          </div>
+          <div className='flex top-4 justify-center py-2'>
+            {slides.map((_slide, slideIndex) => (
+              <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className='text-2xl cursor-pointer'>
+                <RxDotFilled />
+              </div>
+            ))}
+          </div>
         </div>
 
         <div className="row">
